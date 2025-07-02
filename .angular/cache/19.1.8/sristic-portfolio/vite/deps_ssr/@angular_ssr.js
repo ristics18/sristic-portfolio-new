37edff943ca3efd08cc8b1cf861fee1750a2c3d4
surgen <srdjanristic11@gmail.com>
2025-07-01 22:14:01 -0500

    Move all files to root

---
+++ .angular/cache/19.1.8/sristic-portfolio/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,40 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-UI6U5LPL.js";
+import "./chunk-HHCUQPDR.js";
+import "./chunk-MRTKVUOK.js";
+import "./chunk-3TI32C3L.js";
+import "./chunk-TQLFOCE7.js";
+import "./chunk-BGILPUG2.js";
+import "./chunk-CH64OKJE.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
